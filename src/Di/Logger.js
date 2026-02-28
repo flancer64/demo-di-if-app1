fb9a36786cd7244078ef3316aecbef1ba51b1ba2
Alex Gusev <alex@flancer64.com>
2026-02-28 12:10:44 +0200

    Use a closure in the constructor of Client1_Di_Logger

--- src/Di/Logger.js
+++ src/Di/Logger.js
@@ -2,14 +2,18 @@
  * @implements Trans_Api_Logger
  */
 export default class Client1_Di_Logger {
-    #console;
 
-    setConsoleLogger(console) {
-        this.#console = console;
-    }
+    constructor() {
+        /** @type {Console|undefined} */
+        let consoleRef;
 
-    print(msg) {
-        this.#console.log('Client1 logger: ' + msg);
+        this.setConsoleLogger = (console) => {
+            consoleRef = console;
+        };
+
+        this.print = (msg) => {
+            consoleRef.log('Client1 logger: ' + msg);
+        };
     }
 
 }
